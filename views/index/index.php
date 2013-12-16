9df695e48877133f86512c962669e26039ced03e
bayusantoso.mail <bayusantoso.mail@gmail.com>
2013-12-16 10:04:40 +0000

    [UMBARA TRANS TICKETING][BAYU SANTOSO][SENIN, 16 DESEMBER 2013] - WITH BOOTSTRAP THEME

--- views/index/index.php
+++ views/index/index.php
@@ -1,12 +1,5 @@
-<table class="easyui-datagrid" data-options="url:'datagrid_data1.json',method:'get',border:false,singleSelect:true,fit:true,fitColumns:true">
-    <thead>
-        <tr>
-            <th data-options="field:'itemid'" width="80">Item ID</th>
-            <th data-options="field:'productid'" width="100">Product ID</th>
-            <th data-options="field:'listprice',align:'right'" width="80">List Price</th>
-            <th data-options="field:'unitcost',align:'right'" width="80">Unit Cost</th>
-            <th data-options="field:'attr'" width="150">Attribute</th>
-            <th data-options="field:'status',align:'center'" width="50">Status</th>
-        </tr>
-    </thead>
-</table>
+<pre>
+    Aplikasi ini dapat digunakan untuk mempermudah proses pengelolaan informasi dan pendataan pemesanan 
+    tiket pada penyedia jasa travel Umbara Trans 
+    Selamat Menggunakan.
+</pre>
