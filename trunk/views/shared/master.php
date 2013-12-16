4b50106ed06e4741e1d2da28e5e393300316e8f1
bayusantoso.mail@gmail.com <bayusantoso.mail@gmail.com@39ebc247-e85d-2c45-5b66-84fb837ca8a7>
2013-12-16 10:04:40 +0000

    [UMBARA TRANS TICKETING][BAYU SANTOSO][SENIN, 16 DESEMBER 2013] - WITH BOOTSTRAP THEME

--- trunk/views/shared/master.php
+++ trunk/views/shared/master.php
@@ -1,36 +1,224 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Travel Management System</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/res/css/jeasyui_themes/default/easyui.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/res/css/jeasyui_themes/icon.css">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Umbara Trans :: Data Pengguna</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <!--base css styles-->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/scripts/bootstrap/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/scripts/bootstrap/bootstrap/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/scripts/bootstrap/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/scripts/bootstrap/normalize/normalize.css">
+
+        <!--page specific css styles-->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/scripts/bootstrap/data-tables/DT_bootstrap.css" />
+
+        <!--flaty css styles-->
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/css/flaty_themes/flaty.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/res/css/flaty_themes/flaty-responsive.css">
+
+        
         <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/jeasyui/jquery.easyui.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/modernizr/modernizr-2.6.2.min.js"></script>
+		<!--menambahkan icon-->
+        <link rel="shortcut icon" href="favicon.ico">
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/bootstrap/bootstrap/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/nicescroll/jquery.nicescroll.min.js"></script>
+
+        <!--page specific plugin scripts-->
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/bootstrap/data-tables/jquery.dataTables.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/bootstrap/data-tables/DT_bootstrap.js"></script>
+
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.resize.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.pie.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.stack.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.crosshair.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flot/jquery.flot.tooltip.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/sparkline/jquery.sparkline.min.js"></script>
+        
+        <!--flaty scripts-->
+        <script type="text/javascript" src="<?php echo base_url(); ?>/res/scripts/flaty/flaty.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () { 
+                if ($('#main-content').height() < $(window).height()) $('#main-content').height($(window).height()); 
+            });
+        </script>
         <style type="text/css">
-            html, body {
-                height: 100%;
-                width: 100%;
-                margin: 0;
-                padding: 0;
-            }
-            .easyui-layout {
-                margin: 0;
+            #main-content {
                 min-height: 100%;
-                min-width: 100%;
             }
         </style>
     </head>
     <body>
-    <div class="easyui-layout">
-        <div data-options="region:'north'" style="height:100px;vertical-align: middle">
-            <h1>Travel Management System</h1>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <!-- BEGIN Brand -->
+                    <a href="#" class="brand">
+                        <small>
+                            <i class="icon-desktop"></i>
+                            Umbara Trans Ticketing 
+                        </small>
+                    </a>
+                    <!-- END Brand -->
+
+                    <!-- BEGIN Responsive Sidebar Collapse -->
+                    <a href="#" class="btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-reorder"></i>
+                    </a>
+                    <!-- END Responsive Sidebar Collapse -->
+
+                    <!-- BEGIN Navbar Buttons -->
+                    <ul class="nav flaty-nav pull-right">
+                      <!-- BEGIN Button User -->
+                        <li class="nav-header">
+                            <i class="icon-time"></i>
+                            Logined From 20:45
+                        </li>
+                        <li class="divider"></li>
+
+                        <li>
+                            <a href="logout.php">
+                                <i class="icon-off"></i>
+                                Logout
+                            </a>
+                        </li>
+                        <!-- END Button User -->
+                    </ul>
+                    <!-- END Navbar Buttons -->
+                </div><!--/.container-fluid-->
+            </div><!--/.navbar-inner-->
+        </nav>
+        <!-- END Navbar -->
+
+        <!-- BEGIN Container -->
+        <div class="container-fluid" id="main-container">
+            <!-- BEGIN Sidebar -->
+            <div id="sidebar" class="nav-collapse">
+                <!-- BEGIN Navlist -->
+                <ul class="nav nav-list">
+                    <li class="active">
+                        <a href="<?php echo base_url(); ?>">
+                            <i class="icon-home"></i>
+                            <span>Beranda</span>
+                        </a>
+                    </li>
+
+                    <li>
+                         <a href="#" class="dropdown-toggle">
+                             <i class="icon-user" ></i>
+                             <span>Data Pengguna</span>
+                             <b class="arrow icon-angle-right"></b>
+                         </a>
+
+                         <!-- BEGIN Submenu -->
+                         <ul class="submenu">
+                             <li><a href="admin.php">Admin</a></li>
+                             <li><a href="member.php">Member</a></li>
+                         </ul>
+                         <!-- END Submenu -->
+                     </li>
+
+                    <li>
+                        <a href="#" class="dropdown-toggle">
+                            <i class="icon-info"></i>
+                            <span>Info Umbara</span>
+                            <b class="arrow icon-angle-right"></b>
+                        </a>
+
+                        <!-- BEGIN Submenu -->
+                        <ul class="submenu">
+                            <li><a href="armada.php">Armada</a></li>
+                            <li><a href="jadwal.php">Jadwal Keberangkatan</a></li>
+							<li><a href="kota.php">Kota</a></li>
+                            <li><a href="lokasi.php">Lokasi Pool</a></li>
+                            <li><a href="rute.php">Rute</a></li>
+                        </ul>
+                        <!-- END Submenu -->
+                    </li>
+
+                    <li>
+                        <a href="#" class="dropdown-toggle">
+                            <i class="icon-edit"></i>
+                            <span>Berita</span>
+                            <b class="arrow icon-angle-right"></b>
+                        </a>
+
+                        <!-- BEGIN Submenu -->
+                        <ul class="submenu">
+                            <li><a href="berita.php">Kelola Berita</a></li>
+                        </ul>
+                        <!-- END Submenu -->
+                    </li>
+
+                    <li>
+                        <a href="#" class="dropdown-toggle">
+                            <i class="icon-shopping-cart"></i>
+                            <span>Pemesanan</span>
+							<b class="arrow icon-angle-right"></b>
+                        </a>
+						<!-- BEGIN Submenu -->
+                        <ul class="submenu">
+                            <li><a href="pesan.php">Cek Ketersediaan</a></li>
+							<li><a href="pesan2.php">Pesan Tiket</a></li>
+                        </ul>
+                        <!-- END Submenu -->
+                    </li>
+
+                    <li>
+                        <a href="bayar.php">
+                            <i class=" icon-credit-card"></i>
+                            <span>Pembayaran</span>
+                        </a>
+                    </li>
+                </ul>
+                <!-- END Navlist -->
+
+                <!-- BEGIN Sidebar Collapse Button -->
+                <div id="sidebar-collapse" class="visible-desktop">
+                    <i class="icon-double-angle-left"></i>
+                </div>
+                <!-- END Sidebar Collapse Button -->
+            </div>
+            <!-- END Sidebar -->
+
+            <!-- BEGIN Content -->
+            <div id="main-content">
+                <!-- BEGIN Page Title -->
+                <div class="page-title">
+                    <div>
+                        <h1><strong>Selamat Datang</strong></h1>
+                        <h4>Pemesanan Tiket UMBARA TRANS</h4>
+                    </div>
+                </div>
+                <!-- END Page Title -->
+
+                <!-- BEGIN Breadcrumb -->
+                <div id="breadcrumbs">
+                    <ul class="breadcrumb">
+                        <li class="active"><i class="icon-home"></i> Home</li>
+                    </ul>
+                </div>
+                <!-- END Breadcrumb -->
+
+                <!-- BEGIN Main Content -->
+                <div class="row-fluid">
+                    <?php require_once $content; ?>
+                </div>
+                <!-- END Main Content -->
+                
+                <footer style="bottom: 0px;">
+                    <p>2013 © Siti Ulfah Fauziah - UIN Syarif Hidayatullah Jakarta.</p>
+                </footer>
+                <a id="btn-scrollup" class="btn btn-circle btn-large" href="#"><i class="icon-chevron-up"></i></a>
+            </div>
+            <!-- END Content -->
         </div>
-        <div data-options="region:'south',split:true" style="height:50px;"></div>
-        <div data-options="region:'west',split:true" title="Menu" style="width:200px;"></div>
-        <div data-options="region:'center',title:'Main Title',iconCls:'icon-ok'">
-            <?php require_once $content; ?>
-        </div>
-    </div>
     </body>
 </html>
-    
